a38817fa5892ea1ba55726ecbeb415e095fc5262
Chlorie <ChlorieHCl@outlook.com>
2023-10-11 19:29:39 +0800

    First wave of refactoring customizations into members

--- lib/include/clu/query.h
+++ lib/include/clu/query.h
@@ -15,7 +15,7 @@ namespace clu
     {
     };
 
-    namespace detail::qry
+    namespace detail
     {
         struct get_env_t
         {
@@ -26,7 +26,7 @@ namespace clu
                 if constexpr (tag_invocable<get_env_t, T>)
                 {
                     static_assert(queryable<tag_invoke_result_t<get_env_t, T>>, "get_env should return a queryable");
-                    return tag_invoke(*this, static_cast<T&&>(value));
+                    return tag_invoke(get_env_t{}, static_cast<T&&>(value));
                 }
                 else
                     return empty_env{};
@@ -43,7 +43,7 @@ namespace clu
                     static_assert(
                         nothrow_tag_invocable<forwarding_query_t, Qry>, "forwarding_query should be noexcept");
                     static_assert(boolean_testable<tag_invoke_result_t<forwarding_query_t, Qry>>);
-                    return tag_invoke(*this, Qry{}) ? true : false;
+                    return tag_invoke(forwarding_query_t{}, Qry{}) ? true : false;
                 }
                 else
                     return std::derived_from<Qry, forwarding_query_t>;
@@ -54,12 +54,11 @@ namespace clu
         template <typename Qry>
         concept fwd_query = forwarding_query_t{}(Qry{});
         // clang-format on
-    } // namespace detail::qry
+    } // namespace detail
 
-    using detail::qry::forwarding_query_t;
+    using detail::forwarding_query_t;
+    using detail::get_env_t;
     inline constexpr forwarding_query_t forwarding_query{};
-
-    using detail::qry::get_env_t;
     inline constexpr get_env_t get_env{};
 
     // clang-format off
@@ -71,7 +70,7 @@ namespace clu
     template <typename T>
     using env_of_t = call_result_t<get_env_t, T>;
 
-    namespace detail::env_adpt
+    namespace detail
     {
         // TODO: optimize the case of multiple adaptions onto the same environment
 
@@ -85,65 +84,55 @@ namespace clu
         };
 
         template <typename Env, typename... Qs>
-        struct env_t_
-        {
-            class type;
-        };
-
-        template <typename Env, typename... Qs>
-            requires(template_of<Qs, query_value> && ...)
-        using adapted_env_t = typename env_t_<std::remove_cvref_t<Env>, Qs...>::type;
-
-        template <typename Env, typename... Qs>
-        class env_t_<Env, Qs...>::type
+        class adapted_env_t
         {
         public:
             // clang-format off
             template <typename Env2, typename... Ts>
-            constexpr explicit type(Env2&& base, Ts&&... values) noexcept(
+            constexpr explicit adapted_env_t(Env2&& base, Ts&&... values) noexcept(
                 std::is_nothrow_constructible_v<Env, Env2> &&
                 (std::is_nothrow_constructible_v<typename Qs::type, Ts> && ...)):
                 base_(static_cast<Env2&&>(base)),
                 values_(static_cast<Ts&&>(values)...) {}
             // clang-format on
 
-        private:
             using adapted_queries = type_list<typename Qs::query_type...>;
 
-            CLU_NO_UNIQUE_ADDRESS Env base_;
-            CLU_NO_UNIQUE_ADDRESS std::tuple<typename Qs::type...> values_;
-
-            template <qry::fwd_query Qry, typename... Ts>
+            template <fwd_query Qry, typename... Ts>
                 requires(!meta::contains_lv<adapted_queries, Qry>) && //
-                tag_invocable<Qry, const Env&, Ts...> && qry::fwd_query<Qry>
-            constexpr friend decltype(auto) tag_invoke(Qry, const type& self, Ts&&... args) //
-                noexcept(nothrow_tag_invocable<Qry, const Env&, Ts...>)
+                tag_invocable<Qry, const Env&, Ts...> && fwd_query<Qry>
+            constexpr decltype(auto) tag_invoke(Qry, Ts&&... args) //
+                const noexcept(nothrow_tag_invocable<Qry, const Env&, Ts...>)
             {
-                return tag_invoke(Qry{}, self.base_, static_cast<Ts&&>(args)...);
+                return clu::tag_invoke(Qry{}, base_, static_cast<Ts&&>(args)...);
             }
 
             template <typename Qry>
                 requires meta::contains_lv<adapted_queries, Qry>
-            constexpr friend auto tag_invoke(Qry, const type& self) noexcept
+            constexpr auto tag_invoke(Qry) const noexcept
             {
-                return std::get<meta::find_lv<adapted_queries, Qry>>(self.values_);
+                return std::get<meta::find_lv<adapted_queries, Qry>>(values_);
             }
+
+        private:
+            CLU_NO_UNIQUE_ADDRESS Env base_;
+            CLU_NO_UNIQUE_ADDRESS std::tuple<typename Qs::type...> values_;
         };
 
         template <typename Env, typename... Qs>
             requires(template_of<Qs, query_value> && ...)
         constexpr auto adapt_env(Env&& base, Qs&&... qvs) noexcept(
-            std::is_nothrow_constructible_v<adapted_env_t<Env, Qs...>, //
+            std::is_nothrow_constructible_v<adapted_env_t<std::remove_cvref_t<Env>, Qs...>, //
                 Env, typename std::remove_cvref_t<Qs>::type...>)
         {
-            return adapted_env_t<Env, Qs...>( //
+            return adapted_env_t<std::remove_cvref_t<Env>, Qs...>( //
                 static_cast<Env&&>(base), static_cast<Qs&&>(qvs).value...);
         }
-    } // namespace detail::env_adpt
+    } // namespace detail
 
-    using detail::env_adpt::adapt_env;
-    using detail::env_adpt::query_value;
-    using detail::env_adpt::adapted_env_t;
+    using detail::adapt_env;
+    using detail::query_value;
+    using detail::adapted_env_t;
 } // namespace clu
 
 // clang-format off
@@ -152,6 +141,6 @@ namespace clu
  * \param type The query type
  */
 #define CLU_FORWARDING_QUERY(type)                                                                                            \
-    constexpr friend bool tag_invoke(::clu::forwarding_query_t, type) noexcept { return true; }                               \
+    constexpr bool tag_invoke(::clu::forwarding_query_t) const noexcept { return true; }                                      \
     static_assert(true)
 // clang-format on
